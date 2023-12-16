24f8ebcfae3893da6524c9b2c64cde475343620d
marius <99745568+marius-shark@users.noreply.github.com>
2023-12-16 22:52:20 +0100

    adjust script and new data

--- dht_example.py
+++ dht_example.py
@@ -18,7 +18,7 @@ while True:
         humidity = dhtDevice.humidity
         f = open('/home/marius/temperature-monitoring/humidity.csv', 'a+')
         if humidity is not None and temperature_c is not None:
-            f.write('{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature_c, humidity))
+            f.write('{0},{1},{2:0.1f},{3:0.1f}\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature_c, humidity))
         else:
             print("Failed to retrieve data from humidity sensor")
 
